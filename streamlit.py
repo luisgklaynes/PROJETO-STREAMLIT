44fd5ad2a87344d51940a9d27c2cc2398ef27924
Luis <luisguilherme.laynes@gmail.com>
2023-03-22 13:22:19 -0300

    teste

--- streamlit.py
+++ streamlit.py
@@ -1,19 +1,19 @@
 import streamlit as st
 import psycopg2
 
+#CONECTOR COM BANCO
 conn = psycopg2.connect(
     host="localhost",
     database="db_streamlit",
     user="postgres",
     password=";Plansul2021"
 )
+#st.title("USUÁRIOS")
 
+#CONSULTA NO BANCO
 cur = conn.cursor()
-
 cur.execute("SELECT * FROM public.usuarios")
-
 rows = cur.fetchall()
-
 for row in rows:
-    st.write(row)
+    print(row)
     
